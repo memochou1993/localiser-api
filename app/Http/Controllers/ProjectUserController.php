911c6973b511067ed4e1ff068583230e6ee58abd
Memo Chou <memochou1993@gmail.com>
2021-08-19 00:11:22 +0800

    Update error message in ProjectUserController

--- app/Http/Controllers/ProjectUserController.php
+++ app/Http/Controllers/ProjectUserController.php
@@ -9,6 +9,7 @@ use App\Models\Project;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class ProjectUserController extends Controller
 {
@@ -47,13 +48,13 @@ class ProjectUserController extends Controller
     public function destroy(ProjectUserDestroyRequest $request, Project $project, User $user): JsonResponse
     {
         if ($project->users->count() === 1) {
-            abort(Response::HTTP_BAD_REQUEST);
+            throw new AccessDeniedHttpException('This action is unauthorized.');
         }
 
         $admins = $project->users()->wherePivot('role', Role::PROJECT_OWNER)->get();
 
         if ($admins->count() === 1 && $admins->contains($user)) {
-            abort(Response::HTTP_BAD_REQUEST);
+            throw new AccessDeniedHttpException('This action is unauthorized.');
         }
 
         if ($project->users->contains($user)) {
