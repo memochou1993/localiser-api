22d4355ead8cbdc174846f9ae6d4078ef3a7efa8
Memo Chou <memochou1993@gmail.com>
2021-08-23 00:37:03 +0800

    Update requests

--- app/Http/Requests/LanguageStoreRequest.php
+++ app/Http/Requests/LanguageStoreRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\Rule;
 
 class LanguageStoreRequest extends FormRequest
